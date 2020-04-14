a105caca6c1e88c79192a2471b8f009538afd4b5
Atrox <hello@atrox.dev>
2020-04-14 15:32:53 +0200

    add CI

--- project.clj
+++ project.clj
@@ -5,4 +5,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :repl-options {:init-ns haikunator}
 
-  :profiles {:dev {:plugins [[lein-cloverage "1.1.2"]]}})
+  :profiles {:dev {:plugins [[lein-cloverage "1.1.2"]
+                             [lein-kibit "0.1.8"]
+                             [lein-cljfmt "0.6.7"]
+                             [jonase/eastwood "0.3.10"]]}})
